7c5bbd52e2adada5590b4f794280e2810634249f
AliaksandrLutsevich <aliaksandrlutsevich24@gmail.com>
2023-08-06 16:08:07 +0200

    add TransactionList component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Button from "./components/reusable/Button";
 import Input from "./components/reusable/Input";
+import TransactionList from "./components/TransactionList";
+import transactionsData from "./mockups/transactions.json";
 import "./App.css";
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <div className="App">
       <Input type="text" placeholder="Enter value" className="mb-2" />
       <Button onClick={() => console.log("Added")}>Add</Button>
+      <TransactionList transactions={transactionsData} />
     </div>
   );
 }
